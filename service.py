03286ab7c946ee49addf97ee0722c7e0953eb002
tzongw <tzongw@gmail.com>
2019-05-02 11:42:58 +0800

    run

--- service.py
+++ service.py
@@ -1,8 +1,10 @@
 from redis import Redis
-
+import gevent
+import logging
 
 class Service:
     _PREFIX = 'service'
+    _INTERVAL = 5
 
     @classmethod
     def _key_prefix(cls, name):
@@ -12,20 +14,32 @@ class Service:
     def _full_key(cls, name, address):
         return f'{cls._key_prefix(name)}:{address}'
 
-    def __init__(self, redis: Redis):
+    def __init__(self, redis: Redis, services=None, watching=None):
         self._redis = redis
-        self._watched = set()
+        self._services = services
+        self._watching = watching
+        self._runner = None
 
-    def register(self, name, address):
-        key = self._full_key(name, address)
-        self._redis.set(key, '')
+    def start(self):
+        if not self._runner:
+            self._runner = gevent.spawn(self._run)
 
-    def unregister(self, name, address):
-        key = self._full_key(name, address)
-        self._redis.delete(key)
+    def stop(self):
+        if self._runner:
+            gevent.kill(self._runner)
+            self._runner.join()
+            self._runner = None
 
-    def watch(self, name):
-        self._watched.add(name)
+    def _run(self):
+        try:
+            if self._services:
+                pipe = self._redis.pipeline()
+                for name, address in self._services:
+                    key = self._full_key(name, address)
+                    pipe.set(key, '', 3 * self._INTERVAL)
+                pipe.execute()
+        except gevent.GreenletExit:
+            logging.info(f'stopped')
+        except Exception as e:
+            logging.error(f'error: {e}')
 
-    def unwatch(self, name):
-        self._watched.remove(name)
