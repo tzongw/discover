1c6344e3e9cf635d2e48a319f211afe596f663ee
tzongw <tzongw@gmail.com>
2019-06-17 01:04:33 +0800

    test

--- test.py
+++ test.py
@@ -4,10 +4,20 @@ import redis
 import service
 import gevent
 import os
+from tornado import options
+options.parse_command_line()
 
-r = redis.Redis()
-s = service.Service(r, [(os.urandom(4).hex(), os.urandom(4).hex())])
-s.start()
-gevent.sleep(20)
-s.stop()
+
+def main():
+    r = redis.Redis()
+    s = service.Service(r)
+    s.register(os.urandom(4).hex(), os.urandom(4).hex())
+    s.start()
+    gevent.sleep(20)
+    s.stop()
+
+
+if __name__ == "__main__":
+    main()
+
 
