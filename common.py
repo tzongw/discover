46e557789832f89c4e93967c84dc213779861657
tzongw <tzongw@gmail.com>
2019-06-17 21:20:03 +0800

    util

--- common.py
+++ common.py
@@ -4,7 +4,8 @@ from service_pools import ServicePools
 from service import Service
 import const
 from .generated.service import gate, user
-from typing import ContextManager
+from typing import ContextManager, Union
+from functools import partial
 
 
 class _ServicePools(ServicePools):
@@ -17,17 +18,30 @@ class _ServicePools(ServicePools):
             yield user.Client(conn)
 
     @contextlib.contextmanager
-    def gate_client(self) -> ContextManager[gate.Iface]:
-        with self.connection(const.SERVICE_GATE) as conn:
-            yield gate.Client(conn)
-
-    @contextlib.contextmanager
     def address_gate_client(self, address) -> ContextManager[gate.Iface]:
         with self.address_connection(const.SERVICE_GATE, address) as conn:
             yield gate.Client(conn)
 
+    @staticmethod
+    def _one_shot(client_factory, item, *args, **kwargs):
+        with client_factory() as client:
+            return getattr(client, item)(client, *args, **kwargs)
+
+    @staticmethod
+    def _traverse(address_client_factory, addresses, item, *args, **kwargs):
+        for address in addresses:
+            with address_client_factory(address) as client:
+                getattr(client, item)(client, *args, **kwargs)
+
+    def __getattr__(self, item):
+        if hasattr(user.Iface, item):
+            return partial(self._one_shot, self.user_client, item)
+        if hasattr(gate.Iface, item):
+            addresses = self._service.address(const.SERVICE_GATE)
+            return partial(self._traverse, addresses, item)
+        return super().__getattr__(item)
+
 
 redis = Redis()
 service = Service(redis)
-service_pools = _ServicePools(service)
-
+service_pools = _ServicePools(service)  # type: Union[_ServicePools, user.Iface, gate.Iface]
