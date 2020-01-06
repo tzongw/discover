ac8030a3cb0d63239b15c3c8c49cc4dcd360c43b
tzongw <tzongw@gmail.com>
2020-01-07 01:16:25 +0800

    idle

--- executor.py
+++ executor.py
@@ -26,7 +26,7 @@ class _WorkItem:
 
 
 class Executor:
-    def __init__(self, max_workers=None, idle=3600):
+    def __init__(self, max_workers=None, idle=600):
         self._max_workers = max_workers or sys.maxsize
         self._workers = 0
         self._unfinished = 0
