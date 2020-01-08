068f709cdf0a02d2656fe035e02233ab28b40083
tzongw <tzongw@gmail.com>
2020-01-09 00:29:29 +0800

    test

--- aio_test.py
+++ aio_test.py
@@ -14,22 +14,39 @@ parse_command_line()
 one_reader = False
 
 
+
 async def worker(i):
     uid = options.uid + i
     await asyncio.sleep(0.01 * i)
     async with websockets.connect(
-            f'ws://localhost:40001/ws?token=pass&uid={uid}') as websocket:  # type: websockets.WebSocketClientProtocol
+            f'ws://localhost:40001/ws?token=pass&uid={uid}', ping_interval=const.PING_INTERVAL) as websocket:  # type: websockets.WebSocketClientProtocol
         resp = await websocket.recv()  # login resp
-        print(f"{resp} {uid}")
+        await websocket.send('join')
+        print(f"+++{resp} {uid}")
+        ws.append((websocket, uid))
         while True:
-            websocket.ping()
-            await asyncio.sleep(const.PING_INTERVAL)
+            r = await websocket.recv()
+            if not r:
+                print(f'---exit {i}')
+                break
+            if i == 1:
+                print(r)
 
-
+ws = []
+async def sender():
+    from random import choice
+    print('sending')
+    while True:
+        if ws:
+            s, uid = choice(ws)
+            await s.send(f'i am {uid}')
+        await asyncio.sleep(0.1)
+    print('exiting')
 
 fs = []
-for i in range(5000):
+for i in range(1000):
     f = asyncio.ensure_future(worker(i))
     fs.append(f)
 
+asyncio.ensure_future(sender())
 asyncio.get_event_loop().run_until_complete(asyncio.wait(fs))
