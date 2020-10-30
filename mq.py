2d0abd11c3e3e8e65306b6afc8373b211564946e
tzongw <tzongw@gmail.com>
2020-10-30 19:39:43 +0800

    dispatch

--- mq.py
+++ mq.py
@@ -11,30 +11,34 @@ class MQ(Dispatcher):
         super().__init__()
         self._redis = redis
         self._group = group
-        self._stopped = False
-        self._waker = str(uuid.uuid4())
         self._consumer = consumer or str(uuid.uuid4())
+        self._waker = f'waker:{group}:{consumer}'
+        self._stopped = False
 
     def start(self):
         @self.handler(self._waker)
-        def wakeup(data):
-            logging.info(f'{data}')
+        def wakeup(id, data):
+            logging.info(f'{id} {data}')
 
         for stream in self._handlers:
             try:
                 self._redis.xgroup_create(stream, self._group, mkstream=True)
             except ResponseError:
-                pass
+                pass  # group already exists
         gevent.spawn(self._run)
 
     def stop(self):
+        self._stopped = True
         self._redis.xadd(self._waker, {'wake': 'up'})
 
     def _run(self):
         streams = {stream: '>' for stream in self._handlers}
         while not self._stopped:
             try:
-                self._redis.xreadgroup(self._group, self._consumer, streams, noack=True)
+                result = self._redis.xreadgroup(self._group, self._consumer, streams, noack=True)
+                for stream, messages in result:
+                    for message in messages:
+                        self.dispatch(stream, *message)
             except Exception:
                 logging.exception(f'')
                 gevent.sleep(1)
