ed76163285f7637c6284993236c931a8fd9c657e
tzongw <tzongw@gmail.com>
2020-11-08 22:31:54 +0800

    publisher

--- common.py
+++ common.py
@@ -13,6 +13,7 @@ from executor import Executor
 from schedule import Schedule
 from unique import UniqueId
 from utils import Dispatcher, LogSuppress
+from mq import Publisher
 
 LOG_FORMAT = "%(color)s[%(levelname)1.1s %(asctime)s %(module)s:%(funcName)s:%(lineno)d]%(end_color)s %(message)s"
 channel = logging.StreamHandler()
@@ -22,6 +23,7 @@ logger.addHandler(channel)
 
 redis = Redis(decode_responses=True)
 registry = Registry(redis)
+publisher = Publisher(redis)
 user_service = UserService(registry, const.RPC_USER)  # type: Union[UserService, user.Iface]
 gate_service = GateService(registry, const.RPC_GATE)  # type: Union[GateService, gate.Iface]
 timer_service = TimerService(registry, const.RPC_TIMER)  # type: Union[TimerService, timer.Iface]
