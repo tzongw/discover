e33513fc72dc9c70e6f00b81d1357c4e3bf824fc
tzongw <tzongw@gmail.com>
2020-11-28 18:56:02 +0800

    fix log

--- user_server/config.py
+++ user_server/config.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from tornado.options import define, parse_command_line, options
+import common.shared
+from tornado.options import define, options
 from base import utils
 
 define("host", utils.ip_address(), str, "public host")
 define("rpc_port", 0, int, "rpc port")
 
-parse_command_line()
+options.parse_command_line()
