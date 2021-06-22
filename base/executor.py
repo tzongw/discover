70554d1bedf6000e99ac328e82654945e9f1ae66
tzongw <tzongw@gmail.com>
2021-06-22 17:50:18 +0800

    executor available worker

--- base/executor.py
+++ base/executor.py
@@ -32,41 +32,44 @@ class Executor:
     def __init__(self, max_workers=sys.maxsize, idle=600):
         self._max_workers = max_workers
         self._workers = 0
-        self._unfinished = 0
+        self._available_workers = 0
         self._items = queue.Queue()
         self._idle = idle
 
     def submit(self, fn: Callable, *args, **kwargs) -> Future:
         assert callable(fn)
-        self._unfinished += 1
-        logging.debug(f'+job {self}')
-        self._adjust_workers()
         fut = Future()
         item = _WorkItem(fut, fn, *args, **kwargs)
         self._items.put(item)
+        logging.debug(f'+job {self}')
+        self._adjust_workers()
         return fut
 
     def gather(self, *fns):
         return [fut.result() for fut in [self.submit(fn) for fn in fns]]
 
     def _adjust_workers(self):
-        if self._unfinished > self._workers and self._workers < self._max_workers:
+        if len(self._items) > self._available_workers and self._workers < self._max_workers:
             self._workers += 1
+            self._available_workers += 1
             gevent.spawn(self._worker)
             logging.debug(f'+worker {self}')
 
     def __str__(self):
-        return f'unfinished: {self._unfinished}, workers: {self._workers}'
+        return f'pending: {len(self._items)} available: {self._available_workers} workers: {self._workers}'
 
     def _worker(self):
         try:
             while True:
                 item = self._items.get(timeout=self._idle)  # type: _WorkItem
+                self._available_workers -= 1
+                logging.debug(f'get job {self}')
                 item.run()
-                self._unfinished -= 1
+                self._available_workers += 1
                 logging.debug(f'-job {self}')
         except queue.Empty:
             logging.debug(f'worker idle exit')
         finally:
+            self._available_workers -= 1
             self._workers -= 1
             logging.debug(f'-worker {self}')
