5b655d0a4c82b48393e52d02bf23493b83ef7da4
tzongw <tzongw@gmail.com>
2021-07-22 16:47:09 +0800

    config file

--- common/config.py
+++ common/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 from tornado.log import LogFormatter
-from tornado.options import define
+from tornado.options import define, parse_config_file
 
 LOG_FORMAT = "%(color)s[%(levelname)1.1s %(asctime)s %(module)s:%(funcName)s:%(lineno)d]%(end_color)s %(message)s"
 channel = logging.StreamHandler()
@@ -20,5 +20,7 @@ class Addr:
         return f'{self.host}:{self.port}'
 
 
+define("config", type=str, help="path to config file",
+       callback=lambda path: parse_config_file(path, final=False))
 define("redis", "redis://", str, "redis url")
 define("datacenter", 0, int, "data center id")
