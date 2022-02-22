bb329ad775f0f36e88313752f71ca815ed6fd419
tzongw <tzongw@gmail.com>
2022-02-22 12:49:16 +0800

    rename

--- common/task.py
+++ common/task.py
@@ -14,23 +14,23 @@ class AsyncTask:
         self.timer = timer
         self.maxlen = maxlen
         self.handlers = {}
-        self.task_data = local()
+        self.current_task = local()
 
         @receiver.group(Task)
         def handler(id, task: Task):
-            logging.debug(f'got task {id} {task.task_id} {task.path}')
+            logging.debug(f'got task {id} {task.id} {task.path}')
             receiver.redis.xtrim(stream_name(task), minid=id)
             f = self.handlers.get(task.path)
             if not f:  # versioning problem? throw back task, let new version process handle it
-                logging.warning(f'can not handle {id} {task.task_id} {task.path}, stop receive Task')
+                logging.warning(f'can not handle {id} {task.id} {task.path}, stop receive Task')
                 receiver.remove(Task)
                 Publisher(receiver.redis).publish(task, maxlen=self.maxlen)
                 return
             args = pickle.loads(task.args)
             kwargs = pickle.loads(task.kwargs)
-            self.task_data.task_id = task.task_id
+            self.current_task.id = task.id
             f(*args, **kwargs)
-            self.task_data.task_id = None
+            self.current_task.id = None
 
     def __call__(self, f):
         path = f'{f.__module__}.{f.__name__}'
@@ -45,11 +45,11 @@ class AsyncTask:
 
     def post(self, task: Task, interval, loop=False, task_id=None):
         if task_id:
-            task.task_id = task_id
-        elif not task.task_id:
-            task.task_id = f'{stream_name(task)}:{task.path}' if loop else str(uuid.uuid4())
-        return self.timer.create(task, interval, loop, key=task.task_id, maxlen=self.maxlen)
+            task.id = task_id
+        elif not task.id:
+            task.id = f'{stream_name(task)}:{task.path}' if loop else str(uuid.uuid4())
+        return self.timer.create(task, interval, loop, key=task.id, maxlen=self.maxlen)
 
     def cancel(self, task_id=None):
-        assert task_id or self.task_data.task_id
-        return self.timer.kill(task_id or self.task_data.task_id)
+        assert task_id or self.current_task.id
+        return self.timer.kill(task_id or self.current_task.id)
