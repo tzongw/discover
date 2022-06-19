af94c9f66533023bb71cc771d95f39ad56e09c76
tzongw <tzongw@gmail.com>
2022-06-19 22:07:41 +0800

    timer refactor

--- base/timer.py
+++ base/timer.py
@@ -9,31 +9,45 @@ from .utils import stream_name
 
 class Timer:
     _PREFIX = 'timer'
+    _SCRIPT = """#!lua name=timer
+        local function timer_xadd(keys, args)
+          return redis.call('XADD', keys[1], 'MAXLEN', '~', args[2], '*', '', args[1])
+        end
+        
+        local function timer_xadd_hint(keys, args)
+          return redis.call('XADD', keys[1], 'MAXLEN', '~', args[2], 'HINT', args[3], '*', '', args[1])
+        end
+        
+        redis.register_function('timer_xadd', timer_xadd)
+        redis.register_function('timer_xadd_hint', timer_xadd_hint)
+    """
 
     def __init__(self, redis: Redis, cache_key=False, hint=None):
         self.redis = redis
         self.cache_key = cache_key
         self.hint = hint
-        self._script2sha = {}
+        self.registered = False
 
     @classmethod
     def _key(cls, key):
         return f'{cls._PREFIX}:{key}'
 
-    def new(self, key: str, data: str, sha: str, interval: Union[int, timedelta], loop=False):
+    def new(self, key: str, function: str, interval: Union[int, timedelta], loop: bool, num_keys: int,
+            keys_and_args):
         if isinstance(interval, timedelta):
             interval = int(interval.total_seconds() * 1000)
         assert interval >= 1
-        params = [key, data, sha, interval]
+        params = [key, function, interval]
         if loop:
             params.append('LOOP')
+        params.append(num_keys)
+        params += keys_and_args
         key = self._key(key)
         with self.redis.pipeline() as pipe:
             pipe.execute_command('TIMER.NEW', *params)
             if self.cache_key:
                 pipe.hset(key, mapping={
-                    'data': data,
-                    'sha': sha,
+                    'function': function,
                     'interval': interval
                 })
                 if loop:
@@ -53,17 +67,19 @@ class Timer:
 
     def create(self, message: Message, interval: Union[int, timedelta], loop=False, key=None, maxlen=4096,
                do_hint=True):
+        if not self.registered:
+            self.redis.function_load(self._SCRIPT, replace=True)
+            self.registered = True
         stream = stream_name(message)
         data = MessageToJson(message)
         if key is None:
             key = f'{stream}:{data}'
-        hint = f"'HINT', '{self.hint}', " if do_hint and self.hint else ''
-        script = f"return redis.call('XADD', '{stream}', 'MAXLEN', '~', '{maxlen}', {hint} '*', '', ARGV[1])"
-        sha = self._script2sha.get(script)
-        if sha is None:
-            sha = self.redis.script_load(script)
-            self._script2sha[script] = sha
-        self.new(key, data, sha, interval, loop)
+        function = 'timer_xadd'
+        keys_and_args = [stream, data, maxlen]
+        if do_hint and self.hint:
+            function = 'timer_xadd_hint'
+            keys_and_args.append(self.hint)
+        self.new(key, function, interval, loop=loop, num_keys=1, keys_and_args=keys_and_args)
         return key
 
     def exists(self, key: str):
