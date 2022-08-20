b61b31831a67fbbc2a2abac69bac8ab8de3bc43a
tzongw <tzongw@gmail.com>
2022-08-20 22:59:57 +0800

    reload later

--- auto_reload/config.py
+++ auto_reload/config.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import sys
 # noinspection PyUnresolvedReferences
 import common.config
 from tornado.options import define, options
 
-define("conf_d", "/etc/nginx/conf.d", str, "nginx conf dir")
+define("conf_d", "/usr/local/etc/nginx/servers" if sys.platform == "darwin" else "/etc/nginx/conf.d", str,
+       "nginx conf dir")
 
 options.parse_command_line()
--- auto_reload/main.py
+++ auto_reload/main.py
@@ -9,25 +9,36 @@ from common import shared
 import logging
 
 addr_map = {}
+changed = set()
 
 
 def is_api(name: str):
     return name.startswith('ws') or name.startswith('http')
 
 
+def reload_nginx():
+    for name in changed:
+        addrs = addr_map[name]
+        logging.info(f'updating: {name} {addrs}')
+        with open(os.path.join(options.conf_d, name + '_upstream'), 'w', encoding='utf-8') as f:
+            f.write('\n'.join([f'server {l};' for l in addrs]))
+            f.write('\n')
+    changed.clear()
+    os.system("nginx -s reload")
+    logging.info('reload done')
+
+
 def update_upstreams():
-    updated = False
+    empty = not changed
     # noinspection PyProtectedMember
     for name, addrs in shared.registry._addresses.items():
         if is_api(name) and addrs != addr_map.get(name):
+            logging.info(f'changed: {name} {addr_map.get(name)} -> {addrs}')
             addr_map[name] = addrs
-            with open(os.path.join(options.conf_d, name + '_upstream'), 'w', encoding='utf-8') as f:
-                f.write('\n'.join([f'server {l};' for l in addrs]))
-                f.write('\n')
-            updated = True
-            logging.info(f'update {name}')
-    if updated:
-        os.system("nginx -s reload")
+            changed.add(name)
+    if empty and changed:
+        logging.info('spawn reload')
+        gevent.spawn_later(0.2, reload_nginx)
 
 
 def main():
