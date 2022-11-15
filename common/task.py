a77963843b3f036dc550d44dce428460142800c3
tzongw <tzongw@gmail.com>
2022-11-15 23:13:47 +0800

    task handler

--- common/task.py
+++ common/task.py
@@ -18,7 +18,6 @@ class AsyncTask:
     1. can not remove an argument, instead add a new handler
     2. add new argument at the end and set a default value
     """
-    Handler = namedtuple('Handler', ['func', 'params'])
     F = TypeVar('F', bound=Callable)
 
     def __init__(self, timer: Timer, receiver: Receiver, maxlen=4096):
@@ -27,7 +26,6 @@ class AsyncTask:
         self.receiver = receiver
         self.publisher = Publisher(receiver.redis, hint=timer.hint)
         self.maxlen = maxlen
-        self.handlers = {}
         self.local = local()
 
     @staticmethod
@@ -37,25 +35,22 @@ class AsyncTask:
 
     def __call__(self, f: F) -> F:
         path = f'{f.__module__}.{f.__name__}'
-        self.handlers[path] = AsyncTask.Handler(f, signature(f).parameters)
         stream = self.stream_name(Task(path=path))
+        params = signature(f).parameters
 
         @self.receiver.group(Task, stream)
         def handler(id, task: Task):
             logging.debug(f'got task {id} {task.id} {task.path}')
-            h = self.handlers.get(task.path)
             args = json.loads(task.args)  # type: list
             kwargs = json.loads(task.kwargs)  # type: dict
             self.local.task = task
 
             try:
-                params = h.params
-                func = h.func
                 if not any(p.kind == Parameter.VAR_POSITIONAL for p in params.values()):
                     args = args[:len(params)]
                 if not any(p.kind == Parameter.VAR_KEYWORD for p in params.values()):
                     kwargs = {k: v for k, v in kwargs.items() if k in params}
-                func(*args, **kwargs)
+                f(*args, **kwargs)
             finally:
                 del self.local.task
 
