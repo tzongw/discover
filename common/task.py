4c983e79e8aa0135f728bcf2d5fc5400395aa34a
tzongw <tzongw@gmail.com>
2022-12-04 00:52:29 +0800

    heavy task

--- common/task.py
+++ common/task.py
@@ -3,10 +3,14 @@ import json
 import logging
 from typing import TypeVar, Callable
 from gevent.local import local
+from google.protobuf.json_format import MessageToJson, Parse
+from redis import Redis
 from base.mq import Receiver, Publisher
 from base.timer import Timer
 from base.utils import timer_name, var_args
 from .mq_pb2 import Task
+
+F = TypeVar('F', bound=Callable)
 
 
 class AsyncTask:
@@ -15,7 +19,6 @@ class AsyncTask:
     1. can not remove an argument, instead add a new handler
     2. add new argument at the end and set a default value
     """
-    F = TypeVar('F', bound=Callable)
 
     def __init__(self, timer: Timer, receiver: Receiver, maxlen=4096):
         assert timer.redis is receiver.redis
@@ -48,8 +51,8 @@ class AsyncTask:
                 del self.local.task
 
         def wrapper(*args, **kwargs) -> Task:
-            task = Task(path=path, args=json.dumps(args), kwargs=json.dumps(kwargs))
-            task.id = f'{timer_name(task)}:{task.path}:{task.args}:{task.kwargs}'
+            task = Task(id=f'{timer_name(Task)}:{path}:{args}:{kwargs}', path=path, args=json.dumps(args),
+                        kwargs=json.dumps(kwargs))
             return task
 
         return wrapper
@@ -69,3 +72,31 @@ class AsyncTask:
         task = task or self.current_task
         stream = self.stream_name(task)
         self.publisher.publish(task, maxlen=self.maxlen, do_hint=do_hint, stream=stream)
+
+
+class HeavyTask:
+    def __init__(self, redis: Redis, key: str):
+        self.redis = redis
+        self.key = key
+
+    def __call__(self, f: F) -> F:
+        path = f'{f.__module__}.{f.__name__}'
+
+        def wrapper(*args, **kwargs) -> Task:
+            task = Task(id=f'{path}:{args}:{kwargs}', path=path, args=json.dumps(args), kwargs=json.dumps(kwargs))
+            return task
+
+        return wrapper
+
+    def push(self, task: Task):
+        logging.info(f'+task {task.id} {task.path}')
+        self.redis.rpush(self.key, MessageToJson(task))
+
+    def pop(self, *, timeout=0, block=True):
+        r = self.redis.blpop(self.key, timeout) if block else self.redis.lpop(self.key)
+        if r is None:
+            return None
+        value = r[1] if isinstance(r, (list, tuple)) else r
+        task = Parse(value, Task(), ignore_unknown_fields=True)
+        logging.info(f'-task {task.id} {task.path}')
+        return task
