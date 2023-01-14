4a14dc5b1dbdab57052719fd98525d95e157c493
tzongw <tzongw@gmail.com>
2023-01-15 01:44:04 +0800

    timer model

--- timer_server/main.py
+++ timer_server/main.py
@@ -11,119 +11,115 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
 import gevent
-from service import timer
-from typing import Dict
+from service.timer import Processor
+from service.timeout import Client
+from typing import Dict, Callable
 from base.registry import Registry
 from redis import Redis
-from base.schedule import Handle, PeriodicCallback, Schedule
+from base.schedule import PeriodicCallback, Schedule
 from base.service import Service
-from service import timeout
 from setproctitle import setproctitle
-from base.utils import LogSuppress
+from base import LogSuppress, Parser
 import time
+from pydantic import BaseModel
+from dataclasses import dataclass
+
+
+class TimerInfo(BaseModel):
+    key: str
+    service: str
+    data: str
+    deadline = 0.0
+    interval = 0.0
+
+
+@dataclass
+class Timer:
+    info: TimerInfo
+    cancel: Callable
 
 
 class Handler:
     _PREFIX = 'TIMER'
-    _KEY = 'key'
-    _SERVICE = 'service'
-    _DATA = 'data'
-    _DEADLINE = 'deadline'  # one shot
-    _INTERVAL = 'interval'  # repeat
-    _HANDLE = 'handle'
 
     def __init__(self, redis: Redis, schedule: Schedule, registry: Registry):
         self._redis = redis
+        self._parser = Parser(redis)
         self._schedule = schedule
         self._registry = registry
-        self._timers = {}  # type: Dict[str, dict]
+        self._timers = {}  # type: Dict[str, Timer]
         self._services = {}  # type: Dict[str, Service]
 
     def load_timers(self):
         full_keys = set(self._redis.scan_iter(match=f'{self._PREFIX}:*', count=100))
         for full_key in full_keys:
             with LogSuppress(Exception):
-                key, service_name, data, deadline, interval = self._redis.hmget(full_key, self._KEY, self._SERVICE,
-                                                                                self._DATA, self._DEADLINE,
-                                                                                self._INTERVAL)
-                if deadline:
-                    self.call_later(key, service_name, data, float(deadline) - time.time())
-                elif interval:
-                    self.call_repeat(key, service_name, data, float(interval))
+                timer_info = self._parser.get(full_key, TimerInfo)
+                if timer_info.deadline:
+                    self.call_later(timer_info.key, timer_info.service, timer_info.data,
+                                    timer_info.deadline - time.time())
+                elif timer_info.interval:
+                    self.call_repeat(timer_info.key, timer_info.service, timer_info.data, timer_info.interval)
                 else:
-                    logging.error(f'invalid timer: {key} {service_name} {data}')
+                    logging.error(f'invalid timer: {timer_info}')
 
     @classmethod
     def _full_key(cls, key, service_name):
         return f'{cls._PREFIX}:{service_name}:{key}'
 
     def _fire_timer(self, key, service_name, data):
-        logging.info(f'{key} {service_name} {data}')
+        logging.debug(f'{key} {service_name} {data}')
         service = self._services.get(service_name)
         if not service:
             service = Service(self._registry, service_name)
             self._services[service_name] = service
         with service.connection() as conn:
-            client = timeout.Client(conn)
+            client = Client(conn)
             client.timeout(key, data)
 
     def call_later(self, key, service_name, data, delay):
-        deadline = time.time() + delay
-        logging.info(f'{key} {service_name} {data} {deadline}')
-        self._delete_timer(key, service_name)  # delete previous
+        logging.info(f'{key} {service_name} {data} {delay}')
         full_key = self._full_key(key, service_name)
-        timer = {self._KEY: key,
-                 self._SERVICE: service_name,
-                 self._DATA: data,
-                 self._DEADLINE: deadline,
-                 }
-        self._redis.hset(full_key, mapping=timer)
-        self._timers[full_key] = timer
+        self._delete_timer(full_key)
+        deadline = time.time() + delay
+        timer_info = TimerInfo(key=key, service=service_name, data=data, deadline=deadline)
+        self._parser.set(full_key, timer_info)
 
         def callback():
             self.remove_timer(key, service_name)
             self._fire_timer(key, service_name, data)
 
-        timer[self._HANDLE] = self._schedule.call_at(callback, deadline)
+        handle = self._schedule.call_at(callback, deadline)
+        self._timers[full_key] = Timer(info=timer_info, cancel=handle.cancel)
 
     def call_repeat(self, key, service_name, data, interval):
         assert interval > 0
         logging.info(f'{key} {service_name} {data} {interval}')
-        self._delete_timer(key, service_name)  # delete previous
         full_key = self._full_key(key, service_name)
-        timer = {self._KEY: key,
-                 self._SERVICE: service_name,
-                 self._DATA: data,
-                 self._INTERVAL: interval,
-                 }
-        self._redis.hset(full_key, mapping=timer)
-        self._timers[full_key] = timer
+        self._delete_timer(full_key)
+        timer_info = TimerInfo(key=key, service=service_name, data=data, interval=interval)
+        self._parser.set(full_key, timer_info)
 
         def callback():
             self._fire_timer(key, service_name, data)
 
-        timer[self._HANDLE] = PeriodicCallback(self._schedule, callback, interval)
+        pc = PeriodicCallback(self._schedule, callback, interval)
+        self._timers[full_key] = Timer(info=timer_info, cancel=pc.stop)
 
     def remove_timer(self, key, service_name):
         logging.info(f'{key} {service_name}')
         full_key = self._full_key(key, service_name)
         self._redis.delete(full_key)
-        self._delete_timer(key, service_name)
+        self._delete_timer(full_key)
 
-    def _delete_timer(self, key, service_name):
-        full_key = self._full_key(key, service_name)
-        timer = self._timers.pop(full_key, None)
-        if timer:
-            logging.info(f'delete {key} {service_name}')
-            handle = timer[self._HANDLE]
-            if isinstance(handle, Handle):
-                handle.cancel()
-            elif isinstance(handle, PeriodicCallback):
-                handle.stop()
+    def _delete_timer(self, full_key):
+        if timer := self._timers.pop(full_key, None):
+            logging.info(f'delete {full_key}')
+            timer.cancel()
 
 
 def rpc_serve(handler):
-    processor = timer.Processor(handler)
+    processor = Processor(handler)
     transport = TSocket.TServerSocket(port=options.rpc_port)
     tfactory = TTransport.TBufferedTransportFactory()
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
