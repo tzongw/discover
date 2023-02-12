7a4f7427b4c9b5c86ad8ac3ae12733ae90e4ab72
tzongw <tzongw@gmail.com>
2023-02-12 18:47:26 +0800

    rename

--- base/utils.py
+++ base/utils.py
@@ -125,14 +125,15 @@ class DefaultDict(defaultdict):
 
 def zpop_by_score(redis: Redis, key, start, stop, limit=None):
     kwargs = {'offset': 0, 'num': limit} if limit else {}
-    values = {key: score for key, score in redis.zrange(key, start, stop, byscore=True, withscores=True, **kwargs)}
+    members = {member: score for member, score in
+               redis.zrange(key, start, stop, byscore=True, withscores=True, **kwargs)}
     with redis.pipeline() as pipe:
-        for member in values:
+        for member in members:
             pipe.zrem(key, member)
         misses = []
-        for member, removed in zip(values, pipe.execute()):
+        for member, removed in zip(members, pipe.execute()):
             if not removed:
                 misses.append(member)
     for member in misses:
-        values.pop(member)
-    return values
+        members.pop(member)
+    return members
