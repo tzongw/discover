f211625a9430a99dbfdea276debddfd81b468c2a
tzongw <tzongw@gmail.com>
2023-02-19 17:29:54 +0800

    mq

--- base/mq.py
+++ base/mq.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import gevent
 import logging
-import uuid
-from typing import Type, Dict
+from typing import Dict
 from redis import Redis
 from .utils import stream_name, var_args
 from .dispatcher import Dispatcher
@@ -61,10 +60,7 @@ class Receiver:
             executor=Executor(max_workers=batch, queue_size=0, name='group_dispatch'))
         self._fanout_dispatcher = ProtoDispatcher(executor=Executor(max_workers=batch, queue_size=0,
                                                                     name='fanout_dispatch'))
-        self._group_streams = {}
-        self._fanout_streams = {}
         self._batch = batch
-        self._workers = []
 
     @property
     def group(self):
@@ -84,34 +80,30 @@ class Receiver:
             logging.info(f'{sid} {data}')
 
         with self.redis.pipeline() as pipe:
-            for stream in self._group_dispatcher.handlers:
+            streams = set(self._group_dispatcher.handlers) | set(self._fanout_dispatcher.handlers)
+            for stream in streams:
+                # create group & stream
                 pipe.xgroup_create(stream, self._group, mkstream=True)
-            unique_group = str(uuid.uuid4())
-            for stream in self._fanout_dispatcher.handlers:
-                # create empty stream if not exist
-                pipe.xgroup_create(stream, unique_group, mkstream=True)
-                pipe.xgroup_destroy(stream, unique_group)
-            pipe.execute(raise_on_error=False)
-        self._workers = [gevent.spawn(self._group_run), gevent.spawn(self._fanout_run)]
+            pipe.execute(raise_on_error=False)  # group already exists
+        gevent.spawn(self._group_run)
+        gevent.spawn(self._fanout_run)
 
     def stop(self):
         logging.info(f'stop')
         self._stopped = True
         self.redis.xadd(self._waker, {'wake': 'up'})
-        gevent.joinall(self._workers)
         with self.redis.pipeline() as pipe:
             for stream in self._group_dispatcher.handlers:
                 pipe.xgroup_delconsumer(stream, self._group, self._consumer)
             pipe.delete(self._waker)
-            pipe.execute(raise_on_error=False)
-        logging.info(f'delete consumers {self._group_dispatcher.handlers.keys()}')
+            pipe.execute()
         logging.info(f'delete waker {self._waker}')
 
     def _group_run(self):
-        self._group_streams = {stream: '>' for stream in self._group_dispatcher.handlers}
+        streams = {stream: '>' for stream in self._group_dispatcher.handlers}
         while not self._stopped:
             try:
-                result = self.redis.xreadgroup(self._group, self._consumer, self._group_streams, count=self._batch,
+                result = self.redis.xreadgroup(self._group, self._consumer, streams, count=self._batch,
                                                block=0,
                                                noack=True)
                 for stream, messages in result:
@@ -120,12 +112,12 @@ class Receiver:
             except Exception:
                 logging.exception(f'')
                 gevent.sleep(1)
-        logging.info(f'group exit {self._group_streams.keys()}')
+        logging.info(f'group exit {streams.keys()}')
 
     def _fanout_run(self):
         with self.redis.pipeline() as pipe:
-            stream_names = self._fanout_dispatcher.handlers.keys()
-            for stream in stream_names:
+            streams = self._fanout_dispatcher.handlers
+            for stream in streams:
                 pipe.xinfo_stream(stream)
             last_ids = [xinfo['last-generated-id'] for xinfo in pipe.execute()]
         # race happen if use $ as last id
@@ -133,21 +125,15 @@ class Receiver:
         # 2. xadd stream1 message1
         # 3. while handling message1, xadd stream2 message2
         # 4. xread stream1 stream2 $ $, message2 is missing
-        self._fanout_streams = dict(zip(stream_names, last_ids))
+        streams = dict(zip(streams, last_ids))
         while not self._stopped:
             try:
-                result = self.redis.xread(self._fanout_streams, count=self._batch, block=0)
+                result = self.redis.xread(streams, count=self._batch, block=0)
                 for stream, messages in result:
                     for message in messages:
                         self._fanout_dispatcher.dispatch(stream, *message[::-1])
-                        if stream in self._fanout_streams:  # may removed in dispatch
-                            self._fanout_streams[stream] = message[0]  # update last id
+                        streams[stream] = message[0]  # update last id
             except Exception:
                 logging.exception(f'')
                 gevent.sleep(1)
-        logging.info(f'fanout exit {self._fanout_streams.keys()}')
-
-    def remove(self, stream):
-        self._group_streams.pop(stream, None)
-        self._fanout_streams.pop(stream, None)
-        self.redis.xadd(self._waker, {'wake': 'up'}, maxlen=1)
+        logging.info(f'fanout exit {streams.keys()}')
