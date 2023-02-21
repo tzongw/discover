5b873093ffd2dc74fb26aa7fc1c9b5449b23cef2
tzongw <tzongw@gmail.com>
2023-02-21 12:10:17 +0800

    rebalance

--- migrate_slots.py
+++ migrate_slots.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import sys
+from collections import defaultdict
 from enum import Enum, auto
 from redis.cluster import ClusterNode
 from tornado.options import define, options
@@ -8,7 +9,7 @@ from redis import RedisCluster
 from base import Addr
 
 define('source', Addr(':7001'), Addr, 'slot source')
-define('target', Addr(':7002'), Addr, 'slot target')
+define('target', None, Addr, 'slot target')
 define('slot', -1, int, 'slot')
 
 
@@ -28,13 +29,13 @@ def migrate(redis: RedisCluster, source: Addr, target: Addr, slot, confirm=Confi
     if redis.nodes_manager.get_node_from_slot(slot) != source_node:
         raise ValueError(f'source({source}) not own slot({slot})')
     count = redis.cluster_countkeysinslot(slot)
+    logging.info(f'migrate slot({slot} source({source}) -> target({target}) begin')
     if confirm == Confirm.ALWAYS or confirm == Confirm.SOME and count > 0:
         logging.info(f'{count} keys in slot({slot}), continue? (Y/n)')
         answer = sys.stdin.readline()
         if answer.strip() != 'Y':
             logging.info(f'migrate break!!')
             sys.exit(0)
-    logging.info(f'migrate slot({slot} source({source}) -> target({target}) begin')
     source_id = redis.cluster_myid(target_node=source_node)
     target_id = redis.cluster_myid(target_node=target_node)
     redis.cluster_setslot(target_node=target_node, node_id=source_id, slot_id=slot, state='IMPORTING')
@@ -52,17 +53,56 @@ def migrate(redis: RedisCluster, source: Addr, target: Addr, slot, confirm=Confi
     logging.info(f'migrate slot({slot} source({source}) -> target({target}) done')
 
 
+def rebalance(redis: RedisCluster, slots: set):
+    expect = len(slots) // len(redis.get_primaries())
+    owners = {node.name: [] for node in redis.get_primaries()}
+    for slot in slots:
+        node = redis.nodes_manager.get_node_from_slot(slot)
+        owners[node.name].append(slot)
+    with redis.pipeline(transaction=False) as pipe:
+        for slot in slots:
+            pipe.cluster_countkeysinslot(slot)
+        count_keys = dict(zip(slots, pipe.execute()))
+    for node, owned in owners.items():
+        owned.sort(key=lambda slot: count_keys[slot])
+        logging.info(f'node: {node} slots: {len(owned)}')
+    nodes = list(owners)
+    while True:
+        nodes.sort(key=lambda n: len(owners[n]))
+        low: ClusterNode = redis.get_node(node_name=nodes[0])
+        high: ClusterNode = redis.get_node(node_name=nodes[-1])
+        need = expect - len(owners[low.name])
+        can = len(owners[high.name]) - expect
+        assert need >= 0 and can >= 0
+        if need == 0 and can <= 1:
+            logging.info('rebalance done!!')
+            break
+        slot = owners[high.name].pop(0)
+        owners[low.name].append(slot)  # not sorted
+        migrate(redis, source=Addr(high.name), target=Addr(low.name), slot=slot)
+    for node, owned in owners.items():
+        logging.info(f'node: {node} slots: {len(owned)}')
+
+
 def main():
     options.parse_command_line()
-    source: Addr = options.source
-    target: Addr = options.target
+    source = options.source
+    target = options.target
     slot = options.slot
-    if not 0 <= slot < 16384:
-        raise ValueError(f'slot({slot}) out of range')
-    if source == target:
-        raise ValueError(f'source({source}) == target({target})')
     redis = RedisCluster(host=source.host, port=source.port, decode_responses=True)
-    migrate(redis, source, target, slot)
+    if target:
+        if not 0 <= slot < 16384:
+            raise ValueError(f'slot({slot}) out of range')
+        if source == target:
+            raise ValueError(f'source({source}) == target({target})')
+        migrate(redis, source, target, slot)
+    else:
+        if not 0 <= slot <= 16384:
+            raise ValueError(f'slot({slot}) out of range')
+        slots = {redis.keyslot(str(i)) for i in range(len(redis.get_primaries()))}
+        if slot > 0:
+            slots = set(range(slot)) - slots
+        rebalance(redis, slots)
 
 
 if __name__ == '__main__':
