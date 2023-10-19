6498b98a78ec058b9da2331e1be3137f72ae7623
tzongw <tzongw@gmail.com>
2023-10-19 15:10:54 +0800

    - unnecessary reload

--- auto_reload/main.py
+++ auto_reload/main.py
@@ -18,7 +18,7 @@ def is_api(name: str):
     return name.startswith('ws') or name.startswith('http')
 
 
-def valid(addr: str):
+def is_valid(addr: str):
     if addr.startswith('unix://'):
         return os.path.exists(addr[len('unix://'):])
     else:
@@ -27,15 +27,10 @@ def valid(addr: str):
 
 def reload_nginx():
     for name in changed:
-        addrs = sorted(addr for addr in addr_map[name] if valid(addr))
-        if filtered := addr_map[name] - set(addrs):
-            logging.info(f'filtered: {name} {filtered}')
-        if not addrs:
-            logging.info(f'ignore empty: {name}')
-            continue
+        addrs = addr_map[name]
         logging.info(f'updating: {name} {addrs}')
         with open(os.path.join(options.conf_d, name + '_upstream'), 'w', encoding='utf-8') as f:
-            f.write('\n'.join([f'server {l};' for l in addrs]))
+            f.write('\n'.join([f'server {addr};' for addr in addrs]))
             f.write('\n')
     changed.clear()
     os.system("nginx -s reload")
@@ -46,7 +41,8 @@ def update_upstreams():
     empty = not changed
     # noinspection PyProtectedMember
     for name, addrs in shared.registry._addresses.items():
-        if is_api(name) and addrs != addr_map.get(name):
+        addrs = {addr for addr in addrs if is_valid(addr)}
+        if is_api(name) and addrs and addrs != addr_map.get(name):
             logging.info(f'changed: {name} {addr_map.get(name)} -> {addrs}')
             addr_map[name] = addrs
             changed.add(name)
