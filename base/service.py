dba8f28db71d0a827cfa751fe660423f19692639
tzongw <tzongw@gmail.com>
2025-01-04 23:48:07 +0800

    cooldown

--- base/service.py
+++ base/service.py
@@ -19,7 +19,6 @@ class Service:
         self._cooldown = {}  # type: Dict[str, float]
         self._local_addresses = []
         self._good_addresses = []
-        self._all_addresses = []  # same as addresses, but as a list
         registry.add_callback(self._clean_pools)
         self._update_addresses()
         self._reaping = False
@@ -28,30 +27,32 @@ class Service:
         return self._registry.addresses(self._name)
 
     def address(self, hint: str):
-        addresses = self._local_addresses or self._good_addresses or self._all_addresses
+        addresses = self._local_addresses or self._good_addresses
         return addresses[crc32(hint.encode()) % len(addresses)]
 
     @contextlib.contextmanager
     def connection(self, address=None) -> ContextManager[TProtocolBase]:
         if address is None:
-            addresses = self._local_addresses or self._good_addresses or self._all_addresses
+            addresses = self._local_addresses or self._good_addresses
             address = choice(addresses)
-
+        else:
+            if address in self._cooldown and address not in self.addresses():
+                raise ValueError(f'cooling down unhealthy address {address}')
         pool = self._pools[address]
-        with pool.connection() as conn:
-            try:
+        try:
+            with pool.connection() as conn:
                 yield conn
-            except Exception as e:
-                if not ThriftPool.biz_exception(e):
-                    exists = address in self._cooldown
-                    self._cooldown[address] = time.time() + Registry.COOLDOWN
-                    if not exists:
-                        logging.warning(f'+ cool down {self._name} {address}')
-                        interval = self._update_addresses()
-                        if not self._reaping:
-                            self._reaping = True
-                            gevent.spawn_later(interval, self._reap_cooldown)
-                raise
+        except Exception as e:
+            if not ThriftPool.biz_exception(e):
+                exists = address in self._cooldown
+                self._cooldown[address] = time.time() + Registry.COOLDOWN
+                if not exists:
+                    logging.warning(f'+ cool down {self._name} {address}')
+                    interval = self._update_addresses()
+                    if not self._reaping:
+                        self._reaping = True
+                        gevent.spawn_later(interval, self._reap_cooldown)
+            raise
 
     def _clean_pools(self):
         available = self.addresses()
@@ -60,17 +61,16 @@ class Service:
             logging.info(f'clean {self._name} {removed}')
             pool = self._pools.pop(removed)
             pool.close_all()
-            self._cooldown.pop(removed, None)
         self._update_addresses()
 
     def _update_addresses(self):
-        addresses = self._all_addresses = sorted(self.addresses())
         now = time.time()
         expired = [addr for addr, cd in self._cooldown.items() if cd <= now]
         if expired:
             logging.info(f'- cool down {self._name} {expired}')
         for addr in expired:
             self._cooldown.pop(addr)
+        addresses = sorted(self.addresses())
         self._good_addresses = [addr for addr in addresses if addr not in self._cooldown]
         local_host = ip_address()
         self._local_addresses = [addr for addr in self._good_addresses if Addr(addr).host == local_host]
