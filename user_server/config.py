555d1e88373c12657bbc6f44bbe756f9574723b4
tzongw <tzongw@gmail.com>
2025-05-22 23:41:01 +0800

    mongo

--- user_server/config.py
+++ user_server/config.py
@@ -6,6 +6,7 @@ define('back_port', 0, int, 'back port')
 define('init_timer', 'none', str, 'init timer')
 define('tick_timer', False, bool, 'tick timer')
 define('slow_log', 0.0, float, 'slow log threshold, 0 to disable')
+define('mongo', type=str, help='mongo url')
 
 options.app_name = const.APP_USER
 remaining = options.parse_command_line()
