2c4f57679a5e1b878ba3ee5e1d2d4561c5dacf3b
tzongw <tzongw@gmail.com>
2025-10-14 14:51:19 +0800

    process status

--- user_server/const.py
+++ user_server/const.py
@@ -14,8 +14,12 @@ MAX_SESSIONS = 3
 
 class ProcessStatus(IntEnum):
     INIT = 0
-    FAIL = -1
-    SUCCESS = 1
+    APPROVE = 1
+    REJECT = -1
+    CANCEL = -2
+    PROCESSING = 2
+    SUCCESS = 3
+    FAIL = -3
 
 
 class SwitchStatus(IntEnum):
