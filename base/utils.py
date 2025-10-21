f7b767afe192af42dc1e63118be4af2dcbdcacc3
tzongw <tzongw@gmail.com>
2025-10-21 17:41:23 +0800

    string_hash64

--- base/utils.py
+++ base/utils.py
@@ -249,7 +249,7 @@ def create_redis(addr: str):
         return Redis.from_url(f'{proto}://{addr}', decode_responses=True)
 
 
-def string_hash(s: str):
+def string_hash64(s: str):
     digest = hashlib.md5(s.encode()).digest()
     return int.from_bytes(digest[:8], signed=True)
 
