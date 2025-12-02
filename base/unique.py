208f4ab969a93941985a975f92d0f3c47f0c4955
tzongw <tzongw@gmail.com>
2025-12-02 10:28:53 +0800

    UniqueId

--- base/unique.py
+++ base/unique.py
@@ -5,6 +5,7 @@ from itertools import chain
 
 import gevent
 from redis import Redis, RedisCluster
+from redis.commands.core import HashDataPersistOptions
 from .utils import LogSuppress
 
 
@@ -15,32 +16,38 @@ class UniqueId:
 
     def __init__(self, redis: Redis | RedisCluster):
         self._redis = redis
-        self._keys = set()
+        self._keys = {}
         gevent.spawn(self._run)
 
     def gen(self, biz: str, r: range):
+        key = f'{self._PREFIX}:{biz}'
+        assert key not in self._keys
         partition = randrange(r.start, r.stop)
         range_chain = chain(range(partition, r.stop), range(r.start, partition))
-        for id in range_chain:
-            key = f'{self._PREFIX}:{biz}:{id}'
-            if not self._redis.set(key, '', ex=self._TTL, nx=True):
-                logging.info(f'{biz} conflict id {id}, retry next')
-                continue
-            logging.info(f'{biz} got unique id {id}')
-            self._keys.add(key)
-            return id
-        raise ValueError('no id')
+        for unique_id in range_chain:
+            if self._redis.hsetex(key, str(unique_id), '', ex=self._TTL,
+                                  data_persist_option=HashDataPersistOptions.FNX):
+                logging.info(f'{key} got unique id {unique_id}')
+                self._keys[key] = unique_id
+                return unique_id
+            logging.info(f'{key} conflict id {unique_id}, retry next')
+            continue
+        raise ValueError('no unique id available')
 
     def stop(self):
+        if not self._keys:
+            return
         logging.info(f'stop {self._keys}')
-        if self._keys:
-            self._redis.delete(*self._keys)
-            self._keys.clear()
+        with self._redis.pipeline(transaction=False) as pipe:
+            for key, unique_id in self._keys.items():
+                pipe.hdel(key, unique_id)
+            pipe.execute()
+        self._keys.clear()
 
     def _run(self):
         while True:
             gevent.sleep(self._INTERVAL)
             with LogSuppress(), self._redis.pipeline(transaction=False) as pipe:
-                for key in self._keys:
-                    pipe.set(key, '', ex=self._TTL)
+                for key, unique_id in self._keys.items():
+                    pipe.hsetex(key, str(unique_id), '', ex=self._TTL)
                 pipe.execute()
