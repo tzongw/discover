5b7ca18bcf2285e019c17565e2e5ba54c6da7574
tzongw <tzongw@gmail.com>
2026-06-04 11:18:42 +0800

    fix unix sock

--- gate_server/ws.py
+++ gate_server/ws.py
@@ -81,7 +81,8 @@ class Client:
 
     def serve(self):
         self.ws.handler.socket.settimeout(const.WS_TIMEOUT)
-        self.ws.handler.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        if options.http_port:
+            self.ws.handler.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         gevent.spawn(self._writer)
         while message := self.ws.receive():
             addr = shared.user_service.address(hint=self.conn_id)
