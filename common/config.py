579dcefc71f94a73b0c02f5e0ea6b9e46f17cd63
tzongw <tzongw@gmail.com>
2026-09-02 00:47:04 +0800

    env default test

--- common/config.py
+++ common/config.py
@@ -91,7 +91,7 @@ options.add_parse_callback(parse_callback)
 
 define('config', type=str, help='path to config file', callback=lambda path: parse_config_file(path, final=False))
 define('app_name', None, str, 'app name', callback=parse_app_name)
-define('env', Environment.DEV, Environment, 'environment')
+define('env', Environment.TEST, Environment, 'environment')
 define('redis', '', str, 'biz redis addr')
 define('registry', None, str, 'registry redis addr, use biz redis if none')
 define('datacenter', 0, int, 'data center id')
